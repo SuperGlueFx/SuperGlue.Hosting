e04b1fac5208bad5aca9bb496ee8a58438bb4139
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2016-09-01 12:08:34 +0200

    [FEATURE] Added topshelf installer

---
+++ src/SuperGlue.Installers.TopShelf/Program.cs
@@ -0,0 +1,35 @@
+﻿using Topshelf;
+
+namespace SuperGlue.Installers.TopShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var environment = "local";
+
+            HostFactory.Run(x =>
+            {
+                x.AddCommandLineDefinition("appname", y =>
+                {
+                    x.SetInstanceName(y);
+                    x.SetServiceName(y);
+                });
+
+                x.AddCommandLineDefinition("environment", y => environment = y);
+
+                x.ApplyCommandLine(string.Join(" ", args));
+
+                x.Service<SuperGlueApplication>(s =>
+                {
+                    s.ConstructUsing(name => new SuperGlueApplication(environment));
+                    s.WhenStarted(r => r.Start());
+                    s.WhenStopped(r => r.Stop());
+                    s.WhenPaused(r => r.Stop());
+                    s.WhenContinued(r => r.Start());
+                    s.WhenShutdown(r => r.Stop());
+                });
+            });
+        }
+    }
+}
---
+++ src/SuperGlue.Installers.TopShelf/SuperGlueApplication.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using SuperGlue.Configuration;
+
+namespace SuperGlue.Installers.TopShelf
+{
+    public class SuperGlueApplication
+    {
+        private readonly string _environment;
+        private readonly SuperGlueBootstrapper _bootstrapper;
+
+        public SuperGlueApplication(string environment)
+        {
+            _environment = environment;
+
+            _bootstrapper = SuperGlueBootstrapper.Find();
+        }
+
+        public void Start()
+        {
+            //TODO:Handle host arguments
+            _bootstrapper.StartApplications(new Dictionary<string, object>(), _environment, new Dictionary<string, string[]>()).Wait();
+        }
+
+        public void Stop()
+        {
+            _bootstrapper?.ShutDown().Wait();
+        }
+    }
+}
